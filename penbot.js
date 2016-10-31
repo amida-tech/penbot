1c4d2e98ba2cbf3d7d0e6191e09fd13dbf3cf6e3
Matthew McCall <matt@mattmccall.com>
2016-10-31 13:01:35 -0700

    semi functional steal.

--- penbot.js
+++ penbot.js
@@ -108,7 +108,7 @@ controller.hears(keywords.penWho, 'direct_mention', function (bot, message) {
         if (err) {
             bot.botkit.log(err);
         } else {
-            if (penStatus.action === 'up') {
+            if (penStatus.action === 'up' || penStatus.action === 'steal') {
                 getUserData(penStatus.user, function (err, userData) {
                     bot.reply(message, '<@' + message.user + '|' + userData.user.name + '> currently has the pen.');
                 });
@@ -125,7 +125,7 @@ controller.hears(keywords.penDown, 'direct_mention', function (bot, message) {
         if (err) {
             bot.botkit.log(err);
         } else {
-            if (penStatus.action === 'up' && (penStatus.user === message.user)) {
+            if ((penStatus.action === 'up' || penStatus.action === 'steal') && (penStatus.user === message.user)) {
                 common.getData(controller, message.channel, function (err, storedData) {
                     if (err) {
                         bot.botkit.log(err);
@@ -160,7 +160,6 @@ controller.hears(keywords.penHi, 'direct_mention', function (bot, message) {
 
 
 controller.hears(keywords.penSteal, 'direct_mention', function (bot, message) {
-
 
     //Conversation logic in it's own function.
     function haveConversation(userData, penUser, callback) {
@@ -223,9 +222,23 @@ controller.hears(keywords.penSteal, 'direct_mention', function (bot, message) {
                                     if (err) {
                                         bot.botkit.log(err);
                                     } else {
-                                        haveConversation(userData, penUserData, function (err, res) {
-
-
+                                        haveConversation(userData, penUserData, function (err, steal) {
+                                            if (steal) {
+                                                var newEntry = {
+                                                    user: message.user,
+                                                    timestamp: message.ts,
+                                                    action: 'steal'
+                                                };
+                                                common.saveData(controller, message.channel, storedData, newEntry, function (err, res) {
+                                                    if (err) {
+                                                        bot.botkit.log(err);
+                                                    } else {
+                                                        bot.reply(message, '<@' + message.user + '|' + userData.user.name + '> is a dirty thief.');
+                                                    }
+                                                });
+                                            } else {
+                                                bot.reply(message, '...good.');
+                                            }
                                         });
                                     }
                                 });
@@ -239,78 +252,4 @@ controller.hears(keywords.penSteal, 'direct_mention', function (bot, message) {
         }
     });
 
-
-
-
-
-
-
-    /*
-        
-                            controller.storage.users.get(message.user, function (err, user) {
-                                if (user && user.name) {
-                                    bot.reply(message, 'Your name is ' + user.name);
-                                } else {
-                                    bot.startConversation(message, function (err, convo) {
-                                        if (!err) {
-                                            convo.say('I do not know your name yet!');
-                                            convo.ask('What should I call you?', function (response, convo) {
-                                                convo.ask('You want me to call you `' + response.text + '`?', [
-                                                    {
-                                                        pattern: 'yes',
-                                                        callback: function (response, convo) {
-                                                            // since no further messages are queued after this,
-                                                            // the conversation will end naturally with status == 'completed'
-                                                            convo.next();
-                                                        }
-                                                    },
-                                                    {
-                                                        pattern: 'no',
-                                                        callback: function (response, convo) {
-                                                            // stop the conversation. this will cause it to end with status == 'stopped'
-                                                            convo.stop();
-                                                        }
-                                                    },
-                                                    {
-                                                        default: true,
-                                                        callback: function (response, convo) {
-                                                            convo.repeat();
-                                                            convo.next();
-                                                        }
-                                                    }
-                                                ]);
-
-                                                convo.next();
-
-                                            }, {
-                                                'key': 'nickname'
-                                            }); // store the results in a field called nickname
-
-                                            convo.on('end', function (convo) {
-                                                if (convo.status == 'completed') {
-                                                    bot.reply(message, 'OK! I will update my dossier...');
-
-                                                    controller.storage.users.get(message.user, function (err, user) {
-                                                        if (!user) {
-                                                            user = {
-                                                                id: message.user,
-                                                            };
-                                                        }
-                                                        user.name = convo.extractResponse('nickname');
-                                                        controller.storage.users.save(user, function (err, id) {
-                                                            bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-                                                        });
-                                                    });
-
-
-
-                                                } else {
-                                                    // this happens if the conversation ended prematurely for some reason
-                                                    bot.reply(message, 'OK, nevermind!');
-                                                }
-                                            });
-                                        }
-                                    });
-                                }
-                            });*/
 });
